03f7015173b2b87eaaba537453cc159ab7dc2ab4
Andrea G. Aristegui <andrearistegui@gmail.com>
2021-06-01 13:04:16 +0200

    pagination and first orderBy

--- src/components/TableFilter.js
+++ src/components/TableFilter.js
@@ -1,106 +1,142 @@
 import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import T from 'prop-types';
 
 import 'antd/dist/antd.css';
 import { Table } from 'antd';
 
 function TableFilter({ marks }) {
-  const ResourcesRows = () => {
-    let fila = [];
-    let indexTable = 1;
-    if (marks && marks.length > 1) {
-      marks.forEach(mark =>
-        fila.push({
-          key: indexTable++,
-          licencePlate: mark.licencePlate,
-          lat: mark.y,
-          lng: mark.x,
-          model: mark.model,
-        }),
+  const [filter, setFilter] = useState(0);
+  const [pagination, setPagination] = useState(0);
+  console.log('marks.length:', marks.length);
+
+  function elegirOrden(datos) {
+    let lista = [];
+    switch (filter) {
+      case 1:
+        lista = orderString(datos, 'dsc');
+        console.log('lista a devolver despues de case 1:', lista);
+        return lista;
+      case 4:
+        lista = orderString(datos, 'dsc');
+        console.log('lista a devolver despues de case 4:', lista);
+        return lista;
+      case 5:
+        lista = orderNumber(datos, 'asc');
+        console.log('lista a devolver despues de case 5:', lista);
+        return lista;
+      case 8:
+        lista = orderNumber(datos, 'dsc');
+        console.log('lista a devolver despues de case 8:', lista);
+        return lista;
+
+      default:
+        return (lista = datos);
+    }
+  }
+
+  function orderNumber(arrayNumeros, option) {
+    if (option === 'asc') {
+      console.log('dentro de ordenNumber asc');
+      return arrayNumeros.sort((a, b) => b.y - a.y);
+    }
+    if (option === 'dsc') {
+      console.log('dentro de ordenNumber desc');
+      return arrayNumeros.sort((a, b) => a.y - b.y);
+    }
+  }
+
+  function orderString(arrayString, option) {
+    if (option === 'asc') {
+      console.log('dentro de orderString asc');
+      return arrayString.sort((a, b) =>
+        a.licencePlate
+          .toLowerCase()
+          .localeCompare(b.licencePlate.toLowerCase()),
       );
     }
-    return fila;
-  };
-  // const [dataRows, setDataRows] = useState();
-  // console.log('dataRows:', dataRows);
-  //
-  // useEffect(() => {
-  //   setDataRows(ResourcesRows());
-  // }, []);
+    if (option === 'dsc') {
+      console.log('dentro de orderString desc');
+      return arrayString.sort((a, b) =>
+        b.licencePlate
+          .toLowerCase()
+          .localeCompare(a.licencePlate.toLowerCase()),
+      );
+    }
+  }
 
-  let data = ResourcesRows();
-  console.log('data:', data);
-  // const [filas, setFilas] = useState([]);
+  function pintarDiez(datos) {
+    let nuevaLista = [];
+    let indice = pagination;
+    let maxPosition = 10;
+    if (datos.length - pagination < 10) {
+      console.log('estamos en la ultima pagina');
+      maxPosition = datos.length % 10;
+    }
 
-  const pintarFilas = datos => {
-    if (!datos && datos.length <= 1) {
+    for (let i = 0; i < maxPosition; i++) {
+      nuevaLista[i] = datos[indice];
+      console.log(`nuevaLista[${i}]: `, nuevaLista[i]);
+      console.log(`datos[${indice}]`, datos[indice]);
+      indice++;
+    }
+    return nuevaLista;
+  }
+
+  function renderRows(datos) {
+    if (!datos || datos.length <= 1) {
       return console.log('no hay datos');
     }
-    return datos.map(({ licencePlate, model, lat, lng, key }) => (
-      <tr key={key}>
-        <td>{key}</td>
+
+    const listaOrdenada = pintarDiez(elegirOrden(datos));
+
+    return listaOrdenada.map(({ licencePlate, model, x, y, id }) => (
+      <tr key={id}>
         <td>{licencePlate}</td>
-        <td>{lat}</td>
-        <td>{lng}</td>
+        <td>{y}</td>
+        <td>{x}</td>
         <td>{model}</td>
       </tr>
     ));
-  };
-
-  // const renderFilas = () => {
-  //   let data = ResourcesRows();
-  //   console.log('data:', data);
-  //   setFilas(data);
-  //   return filas.map(({ licencePlate, model, lat, lng, key }) => (
-  //     <tr key={key}>
-  //       <td>{key}</td>
-  //       <td>{licencePlate}</td>
-  //       <td>{lat}</td>
-  //       <td>{lng}</td>
-  //       <td>{model}</td>
-  //     </tr>
-  //   ));
-  // };
-
-  function handleOrderNumber(ev) {
-    console.log('evento:', ev);
-    const newData = data.sort(function (a, b) {
-      return b.key - a.key;
-    });
-    console.log('data despues de ordenar:', newData);
-    // setFilas(newData);
   }
-  function handleOrderString(ev) {
-    console.log('evento:', ev);
-  }
-
-  const renderHeader = () => {
-    return (
-      <tr>
-        <th onClick={handleOrderNumber} className="item-ordered">
-          Indice
-        </th>
-        <th onClick={handleOrderString} className="item-ordered">
-          Licence Plate
-        </th>
-        <th onClick={handleOrderNumber} className="item-ordered">
-          Lat
-        </th>
-        <th onClick={handleOrderNumber} className="item-ordered">
-          Lng
-        </th>
-        <th onClick={handleOrderString} className="item-ordered">
-          Model
-        </th>
-      </tr>
-    );
+  const handlePageClick = e => {
+    console.log(e);
+    setPagination(e.selected * 10);
   };
 
   return (
-    <table>
-      {renderHeader()}
-      {pintarFilas(data)}
-    </table>
+    <>
+      <table className="resourcesTable">
+        <tr>
+          <th onClick={() => setFilter(1)} className="item-ordered">
+            Licence Plate
+          </th>
+          <th onClick={() => setFilter(5)} className="item-ordered">
+            Lat
+          </th>
+          <th onClick={() => setFilter(8)} className="item-ordered">
+            Lng
+          </th>
+          <th onClick={() => setFilter(4)} className="item-ordered">
+            Model
+          </th>
+        </tr>
+        {renderRows(marks)}
+      </table>
+      <ReactPaginate
+        previousLabel="<<"
+        nextLabel=">>"
+        breakLabel="..."
+        breakClassName="break-me"
+        pageCount={marks.length / 10}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        containerClassName="pagination"
+        subContainerClassName="pages pagination"
+        activeClassName="active"
+        onPageChange={handlePageClick}
+      />
+    </>
   );
 }
 
