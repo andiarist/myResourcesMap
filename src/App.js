3d8b5cbe9f97d7c728df9ad19917e04a9ed30893
Andrea G. Aristegui <andrearistegui@gmail.com>
2021-06-01 19:23:21 +0200

    Filter Map from NavPannel

--- src/App.js
+++ src/App.js
@@ -3,16 +3,45 @@ import { getResources } from './api/resources';
 
 import MapComponent from './components/MapComponent';
 import TableFilter from './components/TableFilter';
+import NavPannel from './components/NavPannel';
+
 import './App.css';
 
 function App() {
   const [resources, setResources] = useState('');
+  const [resourcesFiltered, setResourcesFiltered] = useState('');
+  const [filters, setFilters] = useState([]);
+
   let params =
     '/api/v1/routers/lisboa/resources?lowerLeftLatLon=38.711046,-9.160096&upperRightLatLon=38.739429,-9.137115&companyZoneIds=545,467,473';
 
   useEffect(() => {
-    getResources(params).then(setResources);
+    getResources(params).then(result => {
+      setResources(result);
+      setResourcesFiltered(result);
+    });
   }, []);
+
+  function handleSubmit(formFilters) {
+    console.log('filtros en App:', formFilters);
+    setFilters(formFilters);
+  }
+
+  useEffect(() => {
+    let filteredByTypes = [];
+    if (resources.length !== 0 && filters.length !== 0) {
+      resources.forEach(element => {
+        filters.forEach(item => {
+          if (element.resourceType === item) filteredByTypes.push(element);
+        });
+      });
+      setResourcesFiltered(filteredByTypes);
+    } else {
+      setResourcesFiltered(resources);
+    }
+  }, [filters]);
+
+  console.log('resourcesFiltered en App:', resourcesFiltered);
 
   return (
     <div className="App layout-wrapper">
@@ -21,7 +50,12 @@ function App() {
       </header>
       <main className="layout-main">
         <section className="layout-section">
-          <MapComponent isMarkerShown marks={resources} />
+          <aside className="aside-3">
+            <NavPannel marks={resources} onSubmit={handleSubmit} />
+          </aside>
+          <article className="article-9">
+            <MapComponent isMarkerShown marks={resourcesFiltered} />
+          </article>
         </section>
 
         <section className="layout-section">
